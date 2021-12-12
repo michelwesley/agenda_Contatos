27127f3c965cc1fbe21fbdeb3400bbc6e1670616
MichelFernandes <mwesleyfernandes15071986@yahoo.com.br>
2021-12-12 16:12:54 -0300

    primerio commit

---
+++ src/main/java/br/itau/agenda/model/Agenda.java
@@ -0,0 +1,50 @@
+package br.itau.agenda.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_agenda")
+public class Agenda {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int codigo;
+
+    @Column(name = "nome", length = 120, nullable = false)
+    private String nome;
+
+    @Column(length = 40, nullable = false, unique = true)
+    private String telefone;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    
+    
+}
